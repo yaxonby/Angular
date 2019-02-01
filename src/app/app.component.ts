10e3ab26566627bcb0f58a64d0950a2a9c28714b
Yura <yaxon@tut.by>
2019-02-01 20:21:26 +0300

    two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  // templateUrl: './app.component.html',
+  template: `
+    <div style="text-align:center">
+      <article>
+        <p>Singup</p>
+        <p>Login:</p>
+        <input type="text" [(ngModel)]="login" />
+        <p>Password:</p>
+        <input type="text" [(ngModel)]="password" />
+        <button (click)="singup(login, password)">Singup</button>
+      </article>
+      <section>
+        <p #elementP>load data</p>
+        <button (click)="load()">load</button>
+      </section>
+      <h1>{{ title }}!</h1>
+    </div>
+  `,
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'my-app';
+  title = "my";
+  login: string;
+  password: string;
+
+  @ViewChild("elementP")
+  elementParagraph: ElementRef;
+
+  singup(login, password) {
+    console.log("login=", this.login, "password=", this.password);
+    console.log(this);
+    console.log(this.elementParagraph.nativeElement.textContent);
+  }
+
+  load() {}
 }
